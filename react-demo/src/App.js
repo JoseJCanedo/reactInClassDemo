6ca0ed150b5d3875f54cdef212d1815dbd400001
JoseJCanedo <canedo_jose@columbusstate.edu>
2020-11-05 04:06:06 +0000

    f

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -7,27 +7,49 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            todoList: 'item2'
+            todoList: ['item2'],
+            doneList: ['one'],
+            task: ''
         }
     }
     render(){
         return (
             <div className="App">
                 <h1>To-do List</h1>
-                <input></input>
-                <button onClick={() =>this.addTodo()}>Add Todo</button>
-                <TaskList title={'Pending Todo'} buttonText={'Done'} tasks={this.state.todoList} buttonFunction={this.removeTodo()} />
-                <TaskList title={'Completed'} buttonText={"Delete"} tasks={'done'} buttonFunction={this.removeTodo()} />
+                <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <input 
+                        type='text'
+                        className='input'
+                        placeholder='Enter Item'
+                        value={this.state.task}
+                        onChange={(e) => this.setState({task: e.target.value})}
+                        />
+                    <button type='submit'>Add Todo</button>
+                </form>
+                <TaskList title={'Pending Todo'} buttonText={'Done'} tasks={this.state.todoList} buttonFunction={this.deleteItem} />
+                <TaskList title={'Completed'} buttonText={"Delete"} tasks={this.state.doneList} buttonFunction={this.markComplete} />
             </div>
         );
     }
-    addTodo(){
-        this.setState({ todoList: 'item1'});
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        this.setState({ todoList: [...this.state.todoList, this.state.task], task: ''});
     }
-    removeTodo(){
-        if(this.state.todoList != null){
-            this.setState({ todoList: null})
+    deleteItem = key => {
+        let taskList = this.state.todoList;
+        var index = taskList.indexOf(key);
+        if (index > -1) {
+            taskList.splice(index, 1);
         }
+        this.setState({todoList: taskList, doneList: [...this.state.doneList, key]});
+    }
+    markComplete = key => {
+        let done = this.state.doneList;
+        var index = done.indexOf(key);
+        if (index > -1) {
+            done.splice(index, 1);
+        }
+        this.setState({doneList:done});
     }
 }
 
--- react-demo/src/TaskList.jsx
+++ react-demo/src/TaskList.jsx
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import TaskItems from './TaskItems';
 
 class TaskList extends Component{
-   
+    constructor(){
+        super()
+        this.state = {
+        }
+    }
     render(){
-        const {title, buttonText, tasks, buttonFunction} =this.props;
+        const {title, tasks, buttonFunction} =this.props;
+        const taskItems = tasks.map((task)=> <TaskItems key={task} tasks={task} buttonFunction={buttonFunction}/>)
         return (
             <div>
                 {title}
-                <TaskItems tasks={tasks}/>
-                <button onClick={() => buttonFunction}>{buttonText}</button>
+                {taskItems}
             </div>
         )
     }
